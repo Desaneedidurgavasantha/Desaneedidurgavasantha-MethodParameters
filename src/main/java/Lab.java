06a0559370ec71666cbabcd2f47bf9c936173bac
Desaneedidurgavasantha <dedurga132@gmail.com>
2025-05-14 14:16:49 +0530

    C1

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -13,7 +13,6 @@ public class Lab {
      * functionality of this method - it will always expect the method to return the same variable that was provided
      * to the method. You can also try experimenting with the following guesses, as one of them will be the correct 
      * answer. Please note that many of these will not compile, as evidenced by a "red line" under your code.
-     *
      * return "hello"; | return String "hello";
      * return hello;   | return String hello;
      * return "polly"; | return String "polly";
